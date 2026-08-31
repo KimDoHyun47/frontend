be1c9424b0b5642247e422b81de24da2decc4153
Kim Do Hyun <77099657+wolfram0407@users.noreply.github.com>
2026-08-31 15:05:39 +0900

    env 수정

--- src/api/cards.js
+++ src/api/cards.js
@@ -1,4 +1,4 @@
-const API_URL = (import.meta.env.API_URL || "http://localhost:4000").replace(/\/$/, "");
+const VITE_API_URL = (import.meta.env.VITE_API_URL || "http://localhost:4000").replace(/\/$/, "");
 
 async function request(path, options = {}) {
   let response;
